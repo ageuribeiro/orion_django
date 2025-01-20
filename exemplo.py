e233287c5a72ca05e3cd583e3dea81144e17612d
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2025-01-20 10:29:50 -0300

    refact: Remove exmple.py

--- exemplo.py
+++
@@ -1,9 +0,0 @@
-'''Descrição de Módulo'''
-
-def minha_funcao():
-    '''Descricao de funçao'''
-    print('Olá Mundo')
-
-def essa_e_minha_funcao():
-    '''Descricao de funcao'''
-    print('Ola')
